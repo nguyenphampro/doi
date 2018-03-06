4d8e27090030be406942c3f64cc006bb69159ac4
Bảo Nguyên <baonguyenyam@gmail.com>
2018-03-06 22:34:33 +0700

    Up

--- js/creative.js
+++ js/creative.js
@@ -1,5 +1,89 @@
+function CanhCamResponsive() {
+  // Set BG Resposive
+  $('[bg-img]').each(function () {
+    var bgimg = $(this).attr('bg-img');
+    $(this).css({
+      "background-image": "url(" + bgimg + ")",
+      "background-position": "center center",
+      "background-size": "cover"
+    });
+  });
+
+  $('[bg-img-responsive]').each(function () {
+    var bgimg = $(this).attr('bg-img-responsive');
+    var bgimgsm = $(this).attr('bg-img-responsive-sm');
+    var bgimgxs = $(this).attr('bg-img-responsive-xs');
+    if ($(window).width() >= 900) {
+      $(this).css({
+        "background-image": "url(" + bgimg + ")",
+        "background-position": "center center",
+        "background-size": "cover"
+      });
+    } else if ($(window).width() < 900 && $(window).width() > 600) {
+      $(this).css({
+        "background-image": "url(" + bgimgsm + ")",
+        "background-position": "center center",
+        "background-size": "cover"
+      });
+    } else {
+      $(this).css({
+        "background-image": "url(" + bgimgxs + ")",
+        "background-position": "center center",
+        "background-size": "cover"
+      });
+    }
+  });
+
+  $('[img-src-responsive]').each(function () {
+    var bgimg2 = $(this).attr('img-src-responsive');
+    var bgimg2sm = $(this).attr('img-src-responsive-sm');
+    var bgimg2xs = $(this).attr('img-src-responsive-xs');
+    if ($(window).width() >= 900) {
+      $(this).attr("src", "" + bgimg2 + "");
+    } else if ($(window).width() < 900 && $(window).width() > 600) {
+      $(this).attr("src", "" + bgimg2sm + "");
+    } else {
+      $(this).attr("src", "" + bgimg2xs + "");
+    }
+  });
+
+};
+
+$(function () {
+    CanhCamResponsive();
+});
+
+$(window).resize(function () {
+    CanhCamResponsive();
+});
+
 (function($) {
   "use strict"; // Start of use strict
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    responsiveClass: true,
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 1,
+        nav: false
+      },
+      1000: {
+        items: 1,
+        nav: false
+      }
+    }
+  })
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
